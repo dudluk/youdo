4676a0ae41a95f7e7338adedfaa0c1a948f6f5d1
dudluk <lukasz.dudek@live.com>
2015-06-06 07:03:36 +0100

    I added username and password property to the user class, I added some simple unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   validates :name,  presence: true
   validates :email, presence: true
+  validates :username, presence: true
+  validates :password_digest, presence: true, length: { minimum: 12 }
+
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,23 +4,50 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  PASSWORD = 'password123!'
 
-  test 'Should user be valid when email and name is not empty' do
-    user = User.new(name: 'Lukasz', email: 'lukasz.dudek@gmail.com')
+  def setup
+    @user = User.new(name: 'Lukasz', email: 'lukasz.dudek@gmail.com', username: 'lukasz', password: PASSWORD)
+  end
 
-    assert user.valid?
+  test 'Should be valid' do
+    assert @user.valid?
   end
 
   test 'Should user not be valid when name is missing' do
-    user = User.new(name: '', email: 'lukasz.dudek@gmail.com')
+    @user.name = ''
 
-    assert_not user.valid?
+    assert_not @user.valid?
   end
 
   test 'Should user not be valid when email is missing' do
-    user = User.new(name: 'Lukasz', email: '')
+    @user.email = ''
 
-    assert_not user.valid?
+    assert_not @user.valid?
+  end
+
+  test 'Should user not be valid when password is missing' do
+    @user.password_digest = ''
+
+    assert_not @user.valid?
+  end
+
+  test 'Should user not be valid when password is too short' do
+    @user.password_digest = 'a' * 11
+
+    assert_not @user.valid?
+  end
+
+  test 'Should user authenticate with correct password' do
+    @user.save
+
+    assert @user.authenticate(PASSWORD)
+  end
+
+  test 'Should not user authenticate with incorrect password' do
+    @user.save
+
+    assert_not @user.authenticate('incorrect')
   end
 
 end
